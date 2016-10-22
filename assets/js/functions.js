d0351321e32b3263c2bcd97c56c6b23adc9d33ec
BL1NKY <tobias.unger1@gmx.de>
2016-10-22 02:15:09 +0200

    mentoring added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,47 @@
 $( document ).ready(function() {
 
-  // Get started!
+    mobileNav();
 
+    mentoringBubbleClick();
+
+    $(window).scroll(function () {
+        youtubeVidScroll();
+        startMentoring();
+    });
 });
+
+
+function youtubeVidScroll() {
+    var wScroll = $(window).scrollTop();
+    $(".video-strip").css("background-position","center -" + wScroll/3 + "px")
+}
+
+function mobileNav() {
+    $(".mobile-nav-toggle").on("click", function () {
+        $(".mobile-nav-toggle, .mobile-nav").toggleClass("is-open");
+    });
+}
+
+function mentoringBubbleClick() {
+    $(".face").on("click", function () {
+        var $this = $(this),
+            faceTop = $this.position().top,
+            vertMath = -(faceTop - 230 + 35);
+        $this.siblings().removeClass("has-bubble-open");
+        $this.parent().css("top", + vertMath + "px");
+        setTimeout(function () {
+            $this.addClass("has-bubble-open");
+        }, 300);
+    });
+}
+
+function startMentoring() {
+    var wScroll = $(window).scrollTop();
+
+    if($("section.mentoring").offset().top - 500 < wScroll) {
+        $(".faces").addClass("launched");
+        setTimeout(function () {
+            $(".face:nth-child(3)").addClass("has-bubble-open");
+        }, 300);
+    }
+}
