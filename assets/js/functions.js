83bad2c9a434ab1b77696e50bd86ff96057a5f1a
BL1NKY <tobias.unger1@gmx.de>
2016-10-23 02:10:20 +0200

    mentoring finished

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,8 +8,10 @@ $( document ).ready(function() {
         youtubeVidScroll();
         startMentoring();
     });
-});
 
+    resize();
+
+});
 
 function youtubeVidScroll() {
     var wScroll = $(window).scrollTop();
@@ -24,24 +26,80 @@ function mobileNav() {
 
 function mentoringBubbleClick() {
     $(".face").on("click", function () {
+
         var $this = $(this),
             faceTop = $this.position().top,
-            vertMath = -(faceTop - 230 + 35);
-        $this.siblings().removeClass("has-bubble-open");
-        $this.parent().css("top", + vertMath + "px");
-        setTimeout(function () {
-            $this.addClass("has-bubble-open");
-        }, 300);
+            vertMath = -1 * (faceTop - 230 + 35),
+            faceLeft = $this.position().left,
+            horizMath = (0 - faceLeft);
+
+            $this.siblings().removeClass("has-bubble-open");
+            if($(window).width() > 640) {
+                $this.parent().css("top", + vertMath + "px");
+            } else {
+                if($this.hasClass("back-btn")){
+                    mentoringNarrowStart();
+                } else {
+                    $this.parent().css("left", + horizMath + "px");
+                }
+            }
+            setTimeout(function () {
+                $this.addClass("has-bubble-open");
+            }, 300);
     });
 }
 
 function startMentoring() {
-    var wScroll = $(window).scrollTop();
+    var wScroll = $(window).scrollTop(),
+        triggerPoint = $("section.mentoring").offset().top - 500;
 
-    if($("section.mentoring").offset().top - 500 < wScroll) {
-        $(".faces").addClass("launched");
-        setTimeout(function () {
-            $(".face:nth-child(3)").addClass("has-bubble-open");
-        }, 300);
+
+    if(triggerPoint < wScroll) {
+        if($(window).width() > 640) {
+            $(".faces").addClass("launched");
+            if(!$(".face").hasClass("has-bubble-open") && !$(this).hasClass("back-btn")) {
+                setTimeout(function () {
+                    $(".face:nth-child(3)").addClass("has-bubble-open");
+                }, 300);
+            }
+        } else {
+            if(!$(".face").hasClass("has-bubble-open")) {
+                $(".face").first().addClass("has-bubble-open");
+            }
+        }
     }
+}
+
+$(window).resize(function () {
+    if($(window).width() > 640) {
+        mentoringWideStart();
+    } else {
+        mentoringNarrowStart();
+    }
+});
+
+function resize() {
+    $(window).resize(function () {
+        if($(window).width() > 640) {
+            mentoringWideStart();
+        } else {
+            mentoringNarrowStart();
+        }
+    });
+}
+
+function mentoringNarrowStart() {
+    $(".faces").css({
+        "top":"230px",
+        "left":"0px"
+    });
+    $(".face").first().addClass("has-bubble-open").siblings().removeClass("has-bubble-open");
+}
+
+function mentoringWideStart() {
+    $(".faces").css({
+        "top":"0px",
+        "left":"0px"
+    });
+    $(".face:nth-child(3)").addClass("has-bubble-open").siblings().removeClass("has-bubble-open");
 }
